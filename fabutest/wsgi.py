463e1a32942e1d56e1ff719e1880699590e41bc9
Tobias McNulty <tobias@caktusgroup.com>
2016-01-20 00:15:27 -0500

    make wsgi app use local_settings

--- fabutest/wsgi.py
+++ fabutest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fabutest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fabutest.local_settings")
 
 application = get_wsgi_application()
